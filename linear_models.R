b28174980ca361c0fb9511d2e4556bc3bdbf8a49
erictallen3 <101523263+erictallen3@users.noreply.github.com>
2022-10-01 08:42:53 -0700

    Update linear_models.R

--- linear_models.R
+++ linear_models.R
@@ -1,51 +1,51 @@
 ##-------------Raw value correlations
-s10 <- stack(eviStack, gppStack)
-v10 <- data.frame(na.omit(values(s10)))
+s1 <- stack(eviStack, gppStack)
+v1 <- data.frame(na.omit(values(s1)))
 
-names(v10) <- c('L10', 'L11')
-eviGpplm <- lm(L10 ~ L11, data=v10)
+names(v1) <- c('L1', 'L2')
+eviGpplm <- lm(L1 ~ L2, data=v1)
 eviGpplm
 summary(eviGpplm)
 
 
-s12 <- stack(spiStack, gppStack)
-v12 <- data.frame(na.omit(values(s12)))
+s2 <- stack(spiStack, gppStack)
+v2 <- data.frame(na.omit(values(s2)))
 
-names(v12) <- c('L13', 'L14')
-spiGpplm <- lm(L13 ~ L14, data=v12)
+names(v2) <- c('L3', 'L4')
+spiGpplm <- lm(L3 ~ L4, data=v2)
 spiGpplm
 summary(spiGpplm)
 
 
-s14 <- stack(spiStack, eviStack)
-v14 <- data.frame(na.omit(values(s14)))
+s4 <- stack(spiStack, eviStack)
+v4 <- data.frame(na.omit(values(s4)))
 
-names(v14) <- c('L15', 'L16')
-spiEvilm <- lm(L16 ~ L15, data=v14)
+names(v4) <- c('L5', 'L6')
+spiEvilm <- lm(L5 ~ L6, data=v4)
 spiEvilm
 summary(spiEvilm)
 
 
 ##---------Coefficient of variation correlations
-s2 <- stack(cvSPIstack, cvEVIstack)
-v2 <- data.frame(na.omit(values(s2)))
-names(v2) <- c('L3', 'L4')
-m2 <- lm(L3 ~ L4, data=v2)
-m2
-summary(m2)
+s12 <- stack(cvSPIstack, cvEVIstack)
+v12 <- data.frame(na.omit(values(s12)))
+names(v12) <- c('L13', 'L14')
+m12 <- lm(L13 ~ L14, data=v12)
+m12
+summary(m12)
 
 
-s2 <- stack(cvSPIstack, cvGPPstack)
-v2 <- data.frame(na.omit(values(s2)))
-names(v2) <- c('L3', 'L4')
-m2 <- lm(L3 ~ L4, data=v2)
-m2
-summary(m2)
+s13 <- stack(cvSPIstack, cvGPPstack)
+v13 <- data.frame(na.omit(values(s13)))
+names(v13) <- c('L15', 'L16')
+m13 <- lm(L15 ~ L16, data=v13)
+m13
+summary(m13)
 
 
-k2 <- stack(cvEVIstack, cvGPPstack)
-j2 <- data.frame(na.omit(values(k2)))
-names(j2) <- c('L3', 'L4')
-p2 <- lm(L3 ~ L4, data=j2)
+k12 <- stack(cvEVIstack, cvGPPstack)
+j12 <- data.frame(na.omit(values(k12)))
+names(j2) <- c('L17', 'L18')
+p2 <- lm(L17 ~ L18, data=j12)
 p2
 summary(p2)
